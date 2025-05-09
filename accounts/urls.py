e32724fe17e954949cb89411641e65e36bbee00b
Md Ahaduzzaman <mdahaduzzamanhridoy@gmail.com>
2025-05-09 17:54:53 +0600

    fixed the template colouring variables

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('signin/', auth_views.LoginView.as_view(template_name='accounts/signin.html'), name='signin'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='signin'), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
+]
--- alasatizah/settings.py
+++ alasatizah/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'compressor',
     'core',
+    'accounts',
 ]
 
 MIDDLEWARE = [
